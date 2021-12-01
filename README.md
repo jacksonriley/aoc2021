dd9ac34aa0c3564602f837773d0b3c6199e933a6
Jackson Riley <jackson.riley@btinternet.com>
2021-12-01 13:20:21 +0000

    Tidy up solution and add README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# aoc2021-haskell
+Advent of Code 2021
+===================
+
+My solutions for [Advent of Code 2021](https://adventofcode.com/2021), in Haskell.
+I'm totally new to Haskell so let's see how this goes...
+
+Input for each day goes in `input/<day>`.
+
+Each day can be run with `./run <day>`.
+This will output the solutions for part 1 and part 2.
+
+Unit tests can be run with `stack test`.
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,20 +1,24 @@
 module Day01 where
+import Data.List (foldl')
 
+-- Find how many depth measurements are greater than the one before.
 day1a :: String -> Int
-day1a = foldr incrementRising 0 . windowsN 1 . readNums
+day1a = foldl' incrementRising 0 . windowsN 1 . map read . lines
 
+-- Find how many depth measurements are greater than the one before, grouping
+-- measurements in threes.
 day1b :: String -> Int
-day1b = foldr incrementRising 0 . windowsN 3 . readNums
+day1b = foldl' incrementRising 0 . windowsN 3 . map read . lines
 
 -- Increment the accumulator if the values increase, otherwise leave unchanged.
-incrementRising :: (Int, Int) -> Int -> Int
-incrementRising (first, second) acc =
+incrementRising :: Int -> (Int, Int) -> Int
+incrementRising acc (first, second) =
   if first < second
     then acc + 1
     else acc
 
-readNums :: String -> [Int]
-readNums = map read . lines
-
+-- To compare windows of length N, you actually only need to compare the first
+-- element of the first window with the last element of the second window.
+-- Therefore, pair up all elements with the element N steps after.
 windowsN :: Int -> [a] -> [(a, a)]
-windowsN n l = zip l $ drop n l
+windowsN n xs = zip xs $ drop n xs

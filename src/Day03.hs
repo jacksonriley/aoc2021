7717526ff8744c80ab176d2ec2dad7459579ef66
Jackson Riley <jackson.riley@btinternet.com>
2021-12-03 22:16:15 +0000

    A little tidier

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,62 +1,47 @@
 module Day03 where
 
-import Data.List (foldl', group, maximumBy, sort, sortBy, transpose)
-
-data Extremum
-  = Most
-  | Least
+import Data.List (foldl', transpose)
 
 day3a :: String -> Int
-day3a = product . map binToDec . calculateGammaAndEpsilon . parse
+day3a input = product $ map binToDec [gamma, epsilon]
+  where
+    columns = transpose $ parse input
+    gamma = map (findCommon (>)) columns
+    epsilon = map (1 -) gamma
 
 day3b :: String -> Int
-day3b input =
-  product $
-  map
-    binToDec
-    [findRating Most 1 . parse $ input, findRating Least 0 . parse $ input]
+day3b input = product $ map binToDec [o2, co2]
+  where
+    nums = parse input
+    o2 = findRating (>=) nums
+    co2 = findRating (<) nums
 
--- Parses e.g. "101\n011" into [[1, 0, 1], [0, 1, 1]]
+-- Parses e.g. "101\n011" into [[1, 0, 1], [0, 1, 1]].
 parse :: String -> [[Int]]
 parse = map (map (read . pure)) . lines
 
--- Calculate gamma and epsilon by using a "is the sum greater than length/2"
--- definition for most common bit
-calculateGammaAndEpsilon :: [[Int]] -> [[Int]]
-calculateGammaAndEpsilon xs = [gamma, epsilon]
-  where
-    gamma = map (findCommon Most undefined) . transpose $ xs
-    epsilon = map (findCommon Least undefined) . transpose $ xs
-
--- Convert binary to decimal - e.g. [1, 0, 1] to 5
+-- Convert binary to decimal - e.g. [1, 0, 1] to 5.
 binToDec :: [Int] -> Int
 binToDec = foldl' f 0
   where
     f acc x = 2 * acc + x
 
--- Find the one number that satisfies all of the bit criteria
-findRating :: Extremum -> Int -> [[Int]] -> [Int]
-findRating extremum bias = go 0
+-- Find the one number that satisfies all of the bit criteria.
+findRating :: (Int -> Int -> Bool) -> [[Int]] -> [Int]
+findRating comp = go 0
   where
     go n input =
-      let filterBit = findCommon extremum bias $ transpose input !! n
+      let filterBit = findCommon comp $ transpose input !! n
        in case filter (\xs -> xs !! n == filterBit) input of
             [answer] -> answer
             remaining -> go (n + 1) remaining
 
--- Find most or least common depending on the extremum passed in. Split ties
--- with the bias.
-findCommon :: Extremum -> Int -> [Int] -> Int
-findCommon extremum bias xs =
+-- Find most or least common bit depending on the comparison operator passed
+-- in.
+findCommon :: (Int -> Int -> Bool) -> [Int] -> Int
+findCommon comp xs =
   let ones = length $ filter (== 1) xs
       zeros = length $ filter (== 0) xs
-   in case ones `compare` zeros of
-        GT ->
-          case extremum of
-            Most -> 1
-            Least -> 0
-        EQ -> bias
-        LT ->
-          case extremum of
-            Most -> 0
-            Least -> 1
+   in if ones `comp` zeros
+        then 1
+        else 0

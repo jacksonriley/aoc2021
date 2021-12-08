e47be2e35b2321eab3670e1fd2d59cf47bf9011e
Jackson Riley <jackson.riley@btinternet.com>
2021-12-08 18:48:15 +0000

    Day 8b - doesn't look pretty but I actually quite like my solution

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,7 @@ import Day04 (day4a, day4b)
 import Day05 (day5a, day5b)
 import Day06 (day6a, day6b)
 import Day07 (day7a, day7b)
+import Day08 (day8a, day8b)
 import Test.Hspec
 import Text.RawString.QQ
 
@@ -20,6 +21,7 @@ main =
     test5
     test6
     test7
+    test8
 
 test1 =
   let input =
@@ -119,3 +121,19 @@ test7 =
    in describe "test7" $ do
         it "a" $ do day7a input `shouldBe` (37 :: Int)
         it "b" $ do day7b input `shouldBe` (168 :: Int)
+
+test8 =
+  let input =
+        [r|be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce|]
+   in describe "test8" $ do
+        it "a" $ do day8a input `shouldBe` (26 :: Int)
+        it "b" $ do day8b input `shouldBe` (61229 :: Int)

853e30bdd6042959d1533d96fe68a617ae2b5ce8
Jackson Riley <jackson.riley@btinternet.com>
2021-12-09 18:55:00 +0000

    Jump on the Taskfile hype train

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,22 +1,19 @@
 module Day09 where
 
-import Data.List (foldl', sortBy)
+import Data.List (foldl', sort)
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
 type Position = (Int, Int)
 
 day9a :: String -> Int
-day9a input = sum $ map (\p -> 1 + tileMap M.! p) lowPoints
+day9a input = sum $ map ((+) 1 . snd) lowPoints
   where
     tileMap = parse input
     lowPoints = findLowPoints tileMap
 
 day9b :: String -> Int
-day9b =
-  product .
-  map S.size .
-  take 3 . sortBy (\a b -> S.size b `compare` S.size a) . findBasins . parse
+day9b = product . take 3 . reverse . sort . map S.size . findBasins . parse
 
 parse :: String -> M.Map Position Int
 parse input =
@@ -30,12 +27,12 @@ adjacent :: Position -> [Position]
 adjacent (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 -- Find all of the low points
-findLowPoints :: M.Map Position Int -> [Position]
+findLowPoints :: M.Map Position Int -> [(Position, Int)]
 findLowPoints tileMap = M.foldlWithKey' go [] tileMap
   where
     go acc pos val =
       if all (higher val) $ adjacent pos
-        then pos : acc
+        then (pos, val) : acc
         else acc
     higher val pos' =
       case M.lookup pos' tileMap of
@@ -45,7 +42,7 @@ findLowPoints tileMap = M.foldlWithKey' go [] tileMap
 -- Find all of the basins by doing a flood fill from each low point.
 findBasins :: M.Map Position Int -> [S.Set Position]
 findBasins tileMap =
-  let lowPoints = findLowPoints tileMap
+  let lowPoints = map fst $ findLowPoints tileMap
       basins =
         foldl' (\acc low -> floodFill tileMap [low] S.empty : acc) [] lowPoints
    in basins

ed72aa2188e9b36e051a26afae13d0e0c6fd873d
Jackson Riley <jackson.riley@btinternet.com>
2021-12-16 08:53:18 +0000

    Speed up with a poor man's priority queue (sorted list)

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,18 +1,15 @@
 module Day15 where
 
-import Data.List (foldl', sortOn)
-
+import Data.List (foldl', insertBy, sortOn)
 import qualified Data.Map.Strict as M
+import Data.Maybe (mapMaybe)
+import Data.Ord (comparing)
 import qualified Data.Set as S
 import Debug.Trace
-import Data.Maybe (mapMaybe)
 
 type Position = (Int, Int)
 
 type CostMap = M.Map Position Int
-
--- Poor man's queue
-type Queue = S.Set Position
 
 type Distances = M.Map Position Int
 
@@ -47,55 +44,41 @@ getGoal costMap = (maxX, maxY)
     maxY = maximum . map snd $ M.keys costMap
 
 manhattan :: Position -> Position -> Int
-manhattan (x, y) (x', y') = abs (x-x') + abs (y-y')
-
-heuristic :: Position -> Position -> Int
-heuristic a b = x where
-  x = manhattan a b
-
-popMin :: Queue -> Distances -> Position -> ((Position, Int), Queue)
-popMin queue distances end = (minimumPos, queue')
-  where
-    filtered = mapMaybe (\pos -> M.lookup pos distances >>= (\d -> Just $ (pos, d + heuristic pos end))) $ S.toList queue
-    (pos, d) = head $ sortOn snd filtered
-    minimumPos = (pos, d - heuristic pos end)
-    queue' = S.delete (fst minimumPos) queue
+manhattan (x, y) (x', y') = abs (x - x') + abs (y - y')
 
 wrap :: Int -> Int
 wrap x = 1 + (x - 1) `mod` 9
 
 constructLargerMap :: CostMap -> CostMap
-constructLargerMap smallMap = bigBoy where
-  nX =  (+1) . maximum . map fst $ M.keys smallMap
-  nY =  (+1) . maximum . map snd $ M.keys smallMap
-  tiles = map (\t -> (t, manhattan (0, 0) t)) [(x, y) | x <- [0..4], y <- [0..4]]
-  bigBoy = foldl' makeTile M.empty tiles
-  makeTile acc ((dx, dy), dv) = M.union acc $ M.fromList $ map (\((x, y), v) -> ((x + nX * dx, y + nY * dy), wrap (v + dv))) $ M.toList smallMap
-
+constructLargerMap smallMap = bigBoy
+  where
+    nX = (+ 1) . maximum . map fst $ M.keys smallMap
+    nY = (+ 1) . maximum . map snd $ M.keys smallMap
+    tiles = map (\t -> (t, manhattan (0, 0) t)) [(x, y) | x <- [0 .. 4], y <- [0 .. 4]]
+    bigBoy = foldl' makeTile M.empty tiles
+    makeTile acc ((dx, dy), dv) =
+      M.union acc $
+      M.fromList $
+      map (\((x, y), v) -> ((x + nX * dx, y + nY * dy), wrap (v + dv))) $ M.toList smallMap
 
 dijkstra :: CostMap -> Int
 dijkstra costMap = endDistances M.! end
   where
     end = getGoal costMap
-    initialQueue = S.singleton (0, 0)
+    initialQueue = [((0, 0), 0)]
     definiteDistances = M.singleton (0, 0) 0
-    endDistances = go initialQueue definiteDistances S.empty
-    go :: Queue -> Distances -> Queue -> Distances
-    go queue distances done =
-      let ((pos, d), queue') = popMin queue distances end
-          ns = filter (`S.notMember` done) $ neighbours pos costMap
-          distances' = foldl' foldNeighbours distances ns
-          foldNeighbours :: Distances -> Position -> Distances
-          foldNeighbours acc n =
+    endDistances = go initialQueue definiteDistances
+    go :: [(Position, Int)] -> Distances -> Distances
+    go queue distances =
+      let ((pos, d):queue') = queue
+          -- Never have to reconsider neighbours as the cost into a given vertex is the same from any neighbour, and if a neighbour is in `distances`, we've already processed it from a closer neighbour
+          ns = filter (`M.notMember` distances) $ neighbours pos costMap
+          (distances', queue'') = foldl' foldNeighbours (distances, queue') ns
+          foldNeighbours ::
+               (Distances, [(Position, Int)]) -> Position -> (Distances, [(Position, Int)])
+          foldNeighbours (foldDists, foldQ) n =
             let nEntry = costMap M.! n
-             in case M.lookup n distances of
-                  Just current ->
-                    if current > d + nEntry
-                      then M.insert n (d + nEntry) acc
-                      else acc
-                  Nothing -> M.insert n (d + nEntry) acc
-          queue'' = foldl' (\acc x -> S.insert x acc) queue' ns
-          done' = S.insert pos done
+             in (M.insert n (d + nEntry) foldDists, insertBy (comparing snd) (n, d + nEntry) foldQ)
        in if pos == end
             then distances'
-            else go queue'' distances' done'
+            else go queue'' distances'
